a3d7361cad9a1566b17f94f7ec461b4bf12349b5
Quang Van <quang@absentsoft.com>
2018-05-26 04:21:39 -0400

    connect to your backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
